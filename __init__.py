e90ac74100700a537c79a2fb8482ad50a495f45e
macbook <ramisphoto>
2024-06-11 15:47:16 +0300

    update toml

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .page_analyzer import app
-
-
-__all__ = ('app')
--- page_analyzer/page_analyzer.py
+++ page_analyzer/page_analyzer.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
