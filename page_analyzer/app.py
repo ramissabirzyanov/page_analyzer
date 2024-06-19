823e3afd363490b56777f368e4174f8e4629ae6c
macbook <ramisphoto>
2024-06-19 15:59:50 +0300

    update action in index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -17,7 +17,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/urls', methods=['POST'])
+@app.route('/', methods=['POST'])
 def add_url():
     url = normalize_url(request.form.get('url'))
     if not validators.url(url) or len(url) > 255:
