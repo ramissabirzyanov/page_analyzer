30aae8e6c3b2e150bee6a7055763f468fcea9924
macbook <ramisphoto>
2024-06-25 14:00:24 +0300

    add connections

--- page_analyzer/data_base.py
+++ page_analyzer/data_base.py
@@ -8,31 +8,39 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
-CONNECTION = psycopg2.connect(DATABASE_URL)
-CONNECTION.autocommit = True
+# CONNECTION = psycopg2.connect(DATABASE_URL)
+# CONNECTION.autocommit = True
 
 
 class URL_DB:
     def get_data_by_name(self, name):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT * FROM urls WHERE urls.name=%s;", (name,))
             data = cursor.fetchone()
         return data
 
     def save_to_db(self, url):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("INSERT INTO urls (name, created_at)\
                            VALUES (%s, NOW())",
                            (url,))
 
     def get_id_by_name(self, name):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT id FROM urls WHERE urls.name=%s;", (name,))
             id = cursor.fetchone()['id']
         return id
 
     def get_urls(self):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT DISTINCT ON (urls.id)\
                            urls.id,\
                            urls.name,\
@@ -49,20 +57,26 @@ class URL_DB:
         return urls
 
     def get_data_by_id(self, id):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT * FROM urls WHERE urls.id=%s;", (id,))
             data = cursor.fetchone()
         return data
 
     def save_check_to_db(self, url_id, code):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("INSERT INTO  url_checks (\
                            url_id, status_code, created_at)\
                            VALUES (%s, %s, NOW());",
                            (url_id, code,))
 
     def get_check_by_url_id(self, url_id):
-        with CONNECTION.cursor(cursor_factory=RealDictCursor) as cursor:
+        connection = psycopg2.connect(DATABASE_URL)
+        connection.autocommit = True
+        with connection.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT * FROM url_checks\
                            WHERE url_checks.url_id=%s\
                            ORDER BY id DESC;", (url_id,))
